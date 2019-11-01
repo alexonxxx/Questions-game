21e7adecd369b1add3c4e6b04b7f46160be67183
alexonxxx <alex.ballo0806@gmail.com>
2019-11-01 19:26:46 +0100

    web app questions game

--- src/main/java/com/ballogomezharo/security/SecurityService.java
+++ src/main/java/com/ballogomezharo/security/SecurityService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
     @Service
     public class SecurityService {
+
         private AuthenticationManager authenticationManager;
         private UserDetailsService userDetailsService;
 
